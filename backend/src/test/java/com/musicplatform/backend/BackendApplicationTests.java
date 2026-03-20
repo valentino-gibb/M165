7acb01b312058c258bd18217dc3db4a06724d5a3
valentino-gibb <vma153556@stud.gibb.ch>
2026-03-20 10:13:44 +0100

    Base Project

---
+++ backend/src/test/java/com/musicplatform/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicplatform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
